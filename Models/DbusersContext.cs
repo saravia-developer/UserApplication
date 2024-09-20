6e63512736feeac2aa73ffdbe580db2a07d09c7e
Luis Saravia <saraviatornero310@gmail.com>
2024-09-20 04:58:18 -0500

    feat(project): Initialed this project

---
+++ Models/DbusersContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserApplication.Models;
+
+public partial class DbusersContext : DbContext
+{
+    public DbusersContext()
+    {
+    }
+
+    public DbusersContext(DbContextOptions<DbusersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3213E83FE4AD3F59");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Dni)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("DNI");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("lastName");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserApplication.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "El apellido es obligatorio")]
+    [StringLength(50, ErrorMessage = "El apellido no puede tener más de 50 caracteres")]
+    public string? LastName { get; set; }
+
+    [Required(ErrorMessage = "El DNI es obligatorio")]
+    [RegularExpression(@"^\d{8}$", ErrorMessage = "El DNI debe ser un número de 8 dígitos")]
+    public string? Dni { get; set; }
+}
